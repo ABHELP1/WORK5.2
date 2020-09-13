cbca33ca73f7343ba257bc5ac25c8221cc71f35a
channasorn chungmarisakul <63010185@KMITL.AC.TH>
2020-09-13 19:16:20 +0700

    Add project files.

---
+++ WORK5.2/Source.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+void Aa();
+char* p, str[100];
+int main() {
+	p = str;
+	scanf("%s", str, 99);//ป้อนได้ไม่เกิน99ตัว
+	Aa();
+}
+void Aa() {
+	while (*p != '\0') {
+		if (*p >= 65 && *p <= 90) {
+			printf("%c", *p);
+		}
+		p++;
+	}
+}
